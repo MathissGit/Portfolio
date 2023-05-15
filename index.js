92fc1d752866e61f7d0f9384340dd4a796c87e1b
mathis <mathissineux@gmail.com>
2023-05-15 16:38:38 +0200

    ajustement minim du code et commentaire

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 if(sessionStorage.getItem("counter") == null)
-        sessionStorage.setItem("counter", "0");
+    sessionStorage.setItem("counter", "0");
 
     let counterValue = Number(sessionStorage.getItem("counter"))
     sessionStorage.setItem("counter",counterValue + 1);
 
     console.log("Counter Value:",  sessionStorage.getItem("counter"));
-    let compteur = document.getElementById('counter');
+    let compteur = document.getElementById("counter");
     
     console.log(compteur)
     compteur.innerHTML = counterValue
